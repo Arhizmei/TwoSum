b9d2aedb0e079041cb58752f9d71916a22039ba7
Arhizmei <arhizmei@gmail.com>
2023-11-26 02:28:27 +0200

    ver.2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,21 @@
 class Solution {
-        fun twoSum(nums: IntArray, target: Int): IntArray {
-                for (i in 0 until nums.size - 1) {
-                        for (j in i + 1 until nums.size) {
-                                if (nums[i] + nums[j] == target) {
-                                        return intArrayOf(i, j)
-                                }
-                        }
-                }
-                // Если не найдено совпадение, вернуть пустой массив или бросить исключение, в зависимости от требований задачи.
-                return intArrayOf()
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val numIndices = HashMap<Int, Int>()
+        for (i in nums.indices) {
+            val complement = target - nums[i]
+            if (numIndices.containsKey(complement)) {
+                return intArrayOf(numIndices[complement]!!, i)
+            }
+            numIndices[nums[i]] = i
         }
+        return intArrayOf()
+    }
 }
 
 fun main() {
-        val solution = Solution()
-        val nums = intArrayOf(2, 7, 11, 15)
-        val target = 9
-        val result = solution.twoSum(nums, target)
-        println(result.contentToString())
+    val solution = Solution()
+    val nums = intArrayOf(11, 15, 2, 9)
+    val target = 9
+    val result = solution.twoSum(nums, target)
+    println(result.contentToString())
 }

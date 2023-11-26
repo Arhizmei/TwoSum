24cdef9981d364ed6619a0c8c4eb31c149cf8752
Arhizmei <arhizmei@gmail.com>
2023-11-26 03:11:40 +0200

    add indicate

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,11 @@ class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
         val numIndices = HashMap<Int, Int>()
         for (i in nums.indices) {
+            val indikator=nums[i]
             val complement = target - nums[i]
+            val indikator3 = numIndices.containsKey(complement)
             if (numIndices.containsKey(complement)) {
+                val indikator2 = intArrayOf(numIndices[complement]!!, i)
                 return intArrayOf(numIndices[complement]!!, i)
             }
             numIndices[nums[i]] = i
@@ -14,7 +17,7 @@ class Solution {
 
 fun main() {
     val solution = Solution()
-    val nums = intArrayOf(11, 15, 2, 9)
+    val nums = intArrayOf(11, 15, 2, 7)
     val target = 9
     val result = solution.twoSum(nums, target)
     println(result.contentToString())
